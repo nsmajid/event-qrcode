f1e457a52818f7c5e827fffbe1215e5154178d20
Nursyam Majid <nursyammajid@gmail.com>
2023-10-14 15:48:18 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ParticipantController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home.index');
+});
+
+Route::resource('event', EventController::class)->except(['show']);
+Route::resource('participant', ParticipantController::class)->except(['show','edit','update']);
